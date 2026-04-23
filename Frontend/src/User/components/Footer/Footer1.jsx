0328785f37aee052d720732c51acf5862b58df33
Kaushal_Vora_8904 <vorakaushal47@gmail.com>
2026-04-23 16:20:21 +0530

    complate home page 1

--- Frontend/src/User/components/Footer/Footer1.jsx
+++ Frontend/src/User/components/Footer/Footer1.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../../../assets/Images/logo-light.png";
 import { CgMenuGridO } from "react-icons/cg";
 import { IoIosCall } from "react-icons/io";
@@ -11,8 +11,15 @@ import { FaVimeoV } from "react-icons/fa";
 import { FaPinterestP } from "react-icons/fa6";
 import footerImg from "../../../assets/Images/footer-one-shape.png";
 import { MdArrowOutward } from "react-icons/md";
+import { IoCallOutline } from "react-icons/io5";
+import { MdOutlineMailOutline } from "react-icons/md";
+import { LuAlarmClock } from "react-icons/lu";
+import { FaPinterest } from "react-icons/fa6";
+import { FaInstagram } from "react-icons/fa6";
+
 
 const Footer1 = () => {
+  const [OpenMenu, setOpenMenu] = useState(false);
   return (
     <section className="Footer min-h-[650px] w-full bg-[#163839]  flex">
       {/* Vertical Logo Strip */}
@@ -21,9 +28,104 @@ const Footer1 = () => {
         <img src={logo} className="rotate-[-90deg] scale-200" />
 
         {/* menu */}
-        <span className="text-white text-3xl cursor-pointer">
+        <button
+          onClick={() => setOpenMenu(true)}
+          className="text-white text-3xl cursor-pointer"
+        >
           <CgMenuGridO />
-        </span>
+        </button>
+
+        {/* overlay */}
+
+        {/* Dark Overlay */}
+        <div
+          className={`fixed inset-0 bg-black/50 z-[99] transition-all duration-500
+ ${OpenMenu ? "opacity-100 visible" : "opacity-0 invisible"}
+ `}
+          onClick={() => setOpenMenu(false)}
+        ></div>
+
+        {/* Offcanvas Panel */}
+      <div
+ className={`
+ fixed top-0 right-0 h-screen w-[330px] bg-[#1d1d1d] z-[100]
+ transform transition-transform duration-500
+ ${OpenMenu ? "translate-x-0" : "translate-x-full"}
+ `}
+>
+          {/* Close */}
+          <button
+            onClick={() => setOpenMenu(false)}
+            className="absolute top-8 right-10 text-white text-xl cursor-pointer px-2 bg-white/30"
+          >
+            ×
+          </button>
+
+          <div className=" text-white mt-3 py-3.5">
+            <div className="border-b  border-white/20 pb-3 px-4">
+              <img src={logo} className="w-45" />
+            </div>
+
+            <div className="content mt-5 px-5 border-b border-white/20">
+              <h2 className="text-2xl font-bold mb-3">About Us</h2>
+              <p className="text-white/70 pb-5 w-full">
+                Consultez is the go-to hub for <br /> early adopters and
+                innovation <br /> enthusiasts, offering cutting <br />
+                technology widely.
+              </p>
+            </div>
+
+            <div className="p-2 mr-20">
+              <div className="py-5 space-y-8 ">
+                <div className="flex flex-row items-start justify-center gap-2">
+                  <span className="text-4xl">
+                    <IoCallOutline />
+                  </span>
+                  <div className="flex flex-col items-start justify-center">
+                    <p className="text-[#C6D936] text-sm mb-2">CALL NOW</p>
+                    <h3 className="text-lg font-medium">+92 (8800) - 98670</h3>
+                  </div>
+                </div>
+
+                <div>
+                  <div className="flex flex-row items-start justify-center gap-2">
+                    <span className="text-4xl">
+                      <MdOutlineMailOutline />
+                    </span>
+                    <div className="flex flex-col items-start justify-center">
+                      <p className="text-[#C6D936] text-sm mb-2">SEND EMAIL</p>
+                      <h3 className="text-lg font-medium">help@company.com</h3>
+                    </div>
+                  </div>
+                </div>
+
+                <div>
+                  <div className="flex flex-row items-start justify-center gap-2">
+                    <span className="text-4xl">
+                      <LuAlarmClock />
+                    </span>
+                    <div className="flex flex-col items-start justify-center gap-2">
+                      <p className="text-[#C6D936] text-sm mx-2">HOURS</p>
+
+                      <div className="flex flex-col gap-4">
+                        <h3 className="text-lg font-medium">
+                          Mon - Sat 8:00 - 6:30,
+                        </h3>
+                        <p className="text-lg font-medium">Sunday - CLOSED</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="left-footer absolute bottom-0 border h-12 w-full border-white/20 flex flex-row items-center justify-evenly text-white text-xl">
+                <span className="border-r border-white/20 pr-8 h-full py-3 duration-300 hover:text-[#1A4137]"><RiTwitterXLine /></span>
+                <span className="border-r border-white/20 pr-8 h-full py-3 duration-300 hover:text-[#1A4137]"><FaFacebookF /></span>
+                <span className="border-r border-white/20 pr-8 h-full py-3 duration-300 hover:text-[#1A4137]"><FaPinterest /></span>
+                <span className="mr-5 duration-300 hover:text-[#1A4137]"><FaInstagram /></span>
+          </div>
+        </div>
       </div>
 
       <div className="left w-[70%] bg-[#163839] flex flex-col">
@@ -168,8 +270,8 @@ const Footer1 = () => {
         </div>
         <div className="hours flex flex-col absolute bottom-30 text-lg text-center">
           <p className="text-[#1A4137] font-bold">09 : 00 AM - 10 : 30 PM</p>
-          <span  className="text-[#1A4137] ">Saturday - Thursday</span>
-        </div>  
+          <span className="text-[#1A4137] ">Saturday - Thursday</span>
+        </div>
       </div>
     </section>
   );
