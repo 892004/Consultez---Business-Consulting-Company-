183a91685c12acf6803f1123ce12f454aef114d8
Kaushal_Vora_8904 <vorakaushal47@gmail.com>
2026-04-23 14:49:06 +0530

    home Page 1 completed

--- Frontend/src/User/components/Footer/Footer1.jsx
+++ Frontend/src/User/components/Footer/Footer1.jsx
@@ -1,11 +1,178 @@
-import React from 'react'
+import React from "react";
+import logo from "../../../assets/Images/logo-light.png";
+import { CgMenuGridO } from "react-icons/cg";
+import { IoIosCall } from "react-icons/io";
+import { MdEmail } from "react-icons/md";
+import { MdLocationPin } from "react-icons/md";
+import { LiaAngleRightSolid } from "react-icons/lia";
+import { FaFacebookF } from "react-icons/fa";
+import { RiTwitterXLine } from "react-icons/ri";
+import { FaVimeoV } from "react-icons/fa";
+import { FaPinterestP } from "react-icons/fa6";
+import footerImg from "../../../assets/Images/footer-one-shape.png";
+import { MdArrowOutward } from "react-icons/md";
 
 const Footer1 = () => {
   return (
-    <section className="Footer min-h-screen w-full bg-red-500 ">
+    <section className="Footer min-h-[650px] w-full bg-[#163839]  flex">
+      {/* Vertical Logo Strip */}
+      <div className="left-container relative w-[110px] border-r border-white/20 flex flex-col items-center justify-center gap-70">
+        {/* Top Logo */}
+        <img src={logo} className="rotate-[-90deg] scale-200" />
 
+        {/* menu */}
+        <span className="text-white text-3xl cursor-pointer">
+          <CgMenuGridO />
+        </span>
+      </div>
+
+      <div className="left w-[70%] bg-[#163839] flex flex-col">
+        <div className="left-top relative h-30 w-full border-b border-white/20 flex flex-row items-center justify-around ">
+          {/* Call */}
+          <div className="call flex flex-row items-center text-white text-xl gap-3">
+            <span className="text-2xl py-3 px-3 bg-white/20 rounded-full">
+              <IoIosCall />
+            </span>
+
+            <div className="call-conatent">
+              <p>Call Us:</p>
+              <p className="font-bold ">+1-2345-2345-54</p>
+            </div>
+          </div>
+
+          {/* Email */}
+          <div className="call flex flex-row items-center text-white text-xl gap-3">
+            <span className="text-2xl py-3 px-3 bg-white/20 rounded-full">
+              <MdEmail />
+            </span>
+
+            <div className="call-conatent">
+              <p>Email Us:</p>
+              <p className="font-bold ">info@help.com</p>
+            </div>
+          </div>
+
+          {/* Hours */}
+          <div className="call flex flex-row items-center text-white text-xl gap-3">
+            <span className="text-2xl py-3 px-3 bg-white/20 rounded-full">
+              <MdLocationPin />
+            </span>
+
+            <div className="call-conatent">
+              <p>Hours:</p>
+              <p className="font-bold ">Daily : 8 AM to 5PM</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="left-middle h-110   w-full flex flex-row items-start justify-around p-20 ">
+          {/* Services */}
+          <div className="service">
+            <h3 className="text-white text-2xl font-semibold">Services</h3>
+            <ul className="text-white flex flex-col gap-5 py-8">
+              <li>Digital Marketing</li>
+              <li>Innovation Space </li>
+              <li>Competitive Analysis</li>
+              <li>Market Research</li>
+              <li>HR Management</li>
+            </ul>
+          </div>
+
+          {/* pages  */}
+          <div className="Pages">
+            <h3 className="text-white text-2xl font-semibold">Pages</h3>
+            <ul className="text-white flex flex-col gap-5 py-8">
+              <li>Our Blog</li>
+              <li>Success Stories</li>
+              <li>Customer Review</li>
+              <li>Contact Us</li>
+              <li>About Us</li>
+            </ul>
+          </div>
+
+          {/* Sign up for  news letter */}
+          <div className="Sign-up ">
+            <h3 className="text-white text-2xl font-semibold">
+              Signup Newsletter
+            </h3>
+
+            <div className="py-6 flex flex-col gap-5">
+              <input
+                type="text"
+                className="bg-white px-4 font-medium text-lg py-3 rounded-full"
+                placeholder="Email Address"
+              />
+              <button className="py-3 w-70 px-19 bg-[#C6D936] rounded-full cursor-pointer flex flex-row items-center gap-2">
+                Sign Up Now <LiaAngleRightSolid />
+              </button>
+            </div>
+
+            <div className="social-icons flex flex-row text-white gap-5 text-lg">
+              <span className="px-3 py-3 bg-white/20 rounded-full duration-500 hover:bg-[#C6D936] hover:text-black">
+                <FaFacebookF />
+              </span>
+              <span className="px-3 py-3 bg-white/20 rounded-full duration-500 hover:bg-[#C6D936] hover:text-black">
+                <RiTwitterXLine />
+              </span>
+              <span className="px-3 py-3 bg-white/20 rounded-full duration-500 hover:bg-[#C6D936] hover:text-black">
+                <FaVimeoV />
+              </span>
+              <span className="px-3 py-3 bg-white/20 rounded-full duration-500 hover:bg-[#C6D936] hover:text-black">
+                <FaPinterestP />
+              </span>
+            </div>
+          </div>
+        </div>
+
+        <div className="left-bottom w-full border-t border-white/10 flex items-start justify-start p-10 ">
+          <span className="text-white/60">
+            @ Copyright 2025. All Right by{" "}
+            <span className="text-[#d5ad4b] hover:underline cursor-pointer">
+              Kodesolution
+            </span>
+          </span>
+        </div>
+      </div>
+
+      <div className="right w-[30%] bg-[#D4F25A] relative flex items-start justify-center">
+        <img
+          src={footerImg}
+          className="h-full w-full object-cover absolute top-0"
+        />
+        <div className="content p-8">
+          <h1 className="text-black text-center text-4xl font-bold py-10">
+            Have a Project in
+            <br /> your Mind?
+          </h1>
+          <div className="contactus group relative flex items-center justify-center gap-2 h-60 w-60 overflow-hidden bg-[#1A4137] ml-5 rounded-full text-xl text-white font-bold z-50 cursor-pointer">
+            Contact us
+            <MdArrowOutward className="transition-transform duration-500 group-hover:rotate-[406deg] " />
+            <div
+              className="
+      bottom-round
+      h-20 
+      w-20 
+      bg-[#C6D936] 
+      rounded-full
+      absolute 
+      bottom-0 
+      right-0
+      transition-all
+      duration-500
+      group-hover:h-60
+      group-hover:w-60
+      group-hover:-z-10
+  "
+            ></div>
+          </div>
+        </div>
+        <div className="hours flex flex-col absolute bottom-30 text-lg text-center">
+          <p className="text-[#1A4137] font-bold">09 : 00 AM - 10 : 30 PM</p>
+          <span  className="text-[#1A4137] ">Saturday - Thursday</span>
+        </div>  
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Footer1
+export default Footer1;
--- Frontend/src/User/components/header/header1/Navbar1.jsx
+++ Frontend/src/User/components/header/header1/Navbar1.jsx
@@ -3,34 +3,228 @@ import Logo from "../../../../assets/Images/logo.png";
 import { Link } from "react-router-dom";
 import { FaAngleDown } from "react-icons/fa6";
 import { IoMdCall } from "react-icons/io";
+import "../header1/header1.css";
 
 const Navbar1 = ({ hideTopbar }) => {
   return (
     <section className="h-[88px] w-full border-b border-gray-300 px-10 bg-white flex items-center justify-between shadow-2xl">
-
       {/* LEFT: Logo */}
       <img src={Logo} alt="" />
 
       {/* RIGHT SIDE */}
       <div className="flex items-center gap-10">
-
         {/* MENU */}
         <ul className="flex items-center gap-10">
-          <Link className="flex items-center gap-2 text-[16px]">
+
+
+
+          {/* HOME PAGE DROP DOWN  */}
+          <Link className="homepage flex items-center gap-2 text-[16px] relative">
             Home <FaAngleDown className="text-[12px]" />
+            <div
+              className="home-dp absolute w-60  bg-white top-8 z-50 shadow-2xl flex flex-col items-start justify-center gap-5 p-4 opacity-0 invisible translate-y-6
+transition-all duration-500 rounded-b-2xl"
+            >
+              <Link
+                className="w-full border-b border-gray-300 pb-2 duration-100 hover:translate-x-2 hover:text-[#1A4137]"
+                to="/"
+              >
+                Home Page 1
+              </Link>
+              <Link
+                className="w-full border-b border-gray-200 pb-1 duration-100 hover:translate-x-2 hover:text-[#1A4137]"
+                to="/homepage2"
+              >
+                Home Page 2
+              </Link>
+              <Link
+                className="w-full border-b border-gray-200 pb-1 duration-100 hover:translate-x-2 hover:text-[#1A4137]"
+                to="/homepage3"
+              >
+                Home Page 3
+              </Link>
+              <Link
+                className="w-full border-b border-gray-200 pb-1 duration-100 hover:translate-x-2 hover:text-[#1A4137]"
+                to="/homepage4"
+              >
+                Home Page 4
+              </Link>
+              <Link
+                className="w-full border-b border-gray-200 pb-1 duration-100 hover:translate-x-2 hover:text-[#1A4137]"
+                to="/homepage5"
+              >
+                Home Page 5
+              </Link>
+              <Link
+                className="w-full border-b border-gray-200 pb-1 duration-100 hover:translate-x-2 hover:text-[#1A4137]"
+                to="/homepage6"
+              >
+                Home Page 6
+              </Link>
+              <Link
+                className="w-full border-b border-gray-200 pb-1 duration-100 hover:translate-x-2 hover:text-[#1A4137]"
+                to="/homepage7"
+              >
+                Home Page 7
+              </Link>
+              <Link
+                className="w-full border-b border-gray-200 pb-1 duration-100 hover:translate-x-2 hover:text-[#1A4137]"
+                to="/single-style"
+              >
+                Single Style
+              </Link>
+              <Link
+                className="w-full border-b border-gray-200 pb-1 duration-100 hover:translate-x-2 hover:text-[#1A4137]"
+                to="/dark-style"
+              >
+                Dark Style{" "}
+              </Link>
+              <Link
+                className="duration-100 hover:translate-x-2 hover:text-[#1A4137]"
+                to="/RTL-style"
+              >
+                RTL Style
+              </Link>
+            </div>
           </Link>
-          <Link className="flex items-center gap-2 text-[16px]">
+
+
+
+
+
+          {/* PAGE DROP DOWN  */}
+
+          <Link className="page flex items-center gap-2 text-[16px] relative">
             Page <FaAngleDown className="text-[12px]" />
+            <div
+              className="page-dp absolute w-50  bg-white top-8 z-50 shadow-2xl flex flex-col items-start justify-center gap-5 p-4 opacity-0 invisible translate-y-6
+transition-all duration-500 rounded-b-2xl"
+            >
+              <Link
+                className="w-full border-b border-gray-300 pb-2 duration-100 hover:translate-x-2 hover:text-[#1A4137]"
+                to="/about"
+              >
+                About
+              </Link>
+              <Link
+                className="w-full border-b border-gray-200 pb-1 duration-100 hover:translate-x-2 hover:text-[#1A4137]"
+                to="/faqs"
+              >
+                Faq
+              </Link>
+              <Link
+                className="w-full border-b border-gray-200 pb-1 duration-100 hover:translate-x-2 hover:text-[#1A4137]"
+                to="/pricing"
+              >
+                Pricing
+              </Link>
+              <Link
+                className="w-full border-b border-gray-200 pb-1 duration-100 hover:translate-x-2 hover:text-[#1A4137]"
+                to="/team"
+              >
+                Team
+              </Link>
+              <Link
+                className="w-full border-b border-gray-200 pb-1 duration-100 hover:translate-x-2 hover:text-[#1A4137]"
+                to="/shop"
+              >
+                Shop
+              </Link>
+              <Link
+                className="w-full border-b border-gray-200 pb-1 duration-100 hover:translate-x-2 hover:text-[#1A4137]"
+                to="/testimonial"
+              >
+                Testimonials
+              </Link>
+              <Link
+                className="w-full duration-100 hover:translate-x-2 hover:text-[#1A4137]"
+                to="/error"
+              >
+                404
+              </Link>
+            </div>
           </Link>
-          <Link className="flex items-center gap-2 text-[16px]">
+
+
+
+
+
+          {/* SERVICE DROP DOWN */}
+
+          <Link className="service flex items-center gap-2 text-[16px] relative">
             Services <FaAngleDown className="text-[12px]" />
+            <div
+              className="service-dp absolute w-50  bg-white top-8 z-50 shadow-2xl flex flex-col items-start justify-center gap-5 p-4 opacity-0 invisible translate-y-6
+transition-all duration-500 rounded-b-2xl"
+            >
+              <Link
+                className="w-full border-b border-gray-300 pb-2 duration-100 hover:translate-x-2 hover:text-[#1A4137]"
+                to="/about"
+              >
+                Services
+              </Link>
+              <Link
+                className="w-full duration-100 hover:translate-x-2 hover:text-[#1A4137]"
+                to="/faqs"
+              >
+                Services details
+              </Link>
+            </div>
           </Link>
-          <Link className="flex items-center gap-2 text-[16px]">
+
+
+
+
+
+
+          {/* PORTFOLIO DROP DOWN */}
+          <Link className="portfolio flex items-center gap-2 text-[16px] relative">
             Portfolio <FaAngleDown className="text-[12px]" />
+
+             <div
+              className="portfolio-dp absolute w-50  bg-white top-8 z-50 shadow-2xl flex flex-col items-start justify-center gap-5 p-4 opacity-0 invisible translate-y-6
+transition-all duration-500 rounded-b-2xl"
+            >
+              <Link
+                className="w-full border-b border-gray-300 pb-2 duration-100 hover:translate-x-2 hover:text-[#1A4137]"
+                to="/about"
+              >
+              Portfolio
+              </Link>
+              <Link
+                className="w-full duration-100 hover:translate-x-2 hover:text-[#1A4137]"
+                to="/faqs"
+              >
+                Portfolio details
+              </Link>
+            </div>            
           </Link>
-          <Link className="flex items-center gap-2 text-[16px]">
+
+          {/* NEWS DROP DOWN */}
+          <Link className="news flex items-center gap-2 text-[16px] relative">
             News <FaAngleDown className="text-[12px]" />
+                <div
+              className="news-dp absolute w-50  bg-white top-8 z-50 shadow-2xl flex flex-col items-start justify-center gap-5 p-4 opacity-0 invisible translate-y-6
+transition-all duration-500 rounded-b-2xl"
+            >
+              <Link
+                className="w-full border-b border-gray-300 pb-2 duration-100 hover:translate-x-2 hover:text-[#1A4137]"
+                to="/about"
+              >
+              News Grid
+              </Link>
+              <Link
+                className="w-full duration-100 hover:translate-x-2 hover:text-[#1A4137]"
+                to="/faqs"
+              >
+                News details
+              </Link>
+            </div>            
           </Link>
+
+
+
+
           <Link className="text-[16px]">Contact</Link>
         </ul>
 
